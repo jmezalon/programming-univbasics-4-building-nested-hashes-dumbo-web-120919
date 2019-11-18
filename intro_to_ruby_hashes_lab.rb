b933230fbc94bc6cfa04dccca632580f821ca0cd
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-18 14:38:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,11 @@ def monopoly_with_third_tier
 	      three_pieces_owned: 100,
 	      four_pieces_owned: 200
 	    },
-	    :names => { longIsland: "LIRR", metropoliton: "MTA", chicagoRail: "CRR", atlantic: "AIRR"}
+	    :names => { 
+	      :reading_railroad => {}, 
+	      metropoliton: "MTA", 
+	      chicagoRail: "CRR", 
+	      atlantic: "AIRR"}
 	  } 
 	}
 end
