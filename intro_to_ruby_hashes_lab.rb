eb2f4e32065232888bae8b0baa7919c5e14d7c1d
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-18 14:48:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,10 +55,11 @@ def monopoly_with_fourth_tier
 	      four_pieces_owned: 200
 	    },
 	    :names => { 
-	      :reading_railroad => {mortgage_value: "$100"}, 
+	      :reading_railroad => {}, 
 	      :pennsylvania_railroad => {}, 
 	      :b_and_o_railroad => {}, 
-	      :shortline_railroad => {}
+	      :shortline_railroad => {},
+	      "mortgage_value": "$100"
 	    }
 	  } 
 	}
