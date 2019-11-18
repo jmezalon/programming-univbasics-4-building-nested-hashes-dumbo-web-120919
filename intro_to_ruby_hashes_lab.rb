5191f276f0fecfa3ba249fa2305305a10312b997
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-18 14:28:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    pieces: 4, 
 	    :rent_in_dollars => {
-	      usd: 92,
+	      names: 92,
 	      pesso: 23,
 	      euro: 29,
 	      yin: 929
